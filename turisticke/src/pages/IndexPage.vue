cd033a5f4e4360724ca1a745a7dff58eb1a496a6
unknown <matijasickristian@gmail.com>
2023-03-29 18:06:30 +0200

    prvi front-end 3

--- turisticke/src/pages/IndexPage.vue
+++ turisticke/src/pages/IndexPage.vue
@@ -4,32 +4,29 @@
     <q-page>
       <q-layout>
         <q-layout-header>
-          <div class="q-pa-md">
-            <q-card>
-              <q-card-section>
-                <div></div>
-                <div class="text-h6">Naziv atrakcije:</div>
-                <div class="text-subtitle1">{{ naziv }}</div>
-              </q-card-section>
-              <q-card-section>
-                <div class="text-h6">Opis atrakcije:</div>
-                <div class="text-subtitle1">{{ opis }}</div>
-              </q-card-section>
-              <q-card-section>
-                <q-btn
-                  @click="$router.push('/komentari')"
-                  label="Pogledaj komentare"
-                />
-              </q-card-section>
-              <q-card-section>
-                <div class="text-h6">Ocjena atrakcije:</div>
-                <div class="text-subtitle1">{{ ocjena }}</div>
-              </q-card-section>
-            </q-card>
-          </div>
+          <q-card class="q-pa-xl" flat bordered>
+            <q-card-section>
+              <div class="text-h6">Naziv atrakcije:</div>
+              <div class="text-subtitle1">{{ naziv }}</div>
+            </q-card-section>
+            <q-card-section>
+              <div class="text-h6">Opis atrakcije:</div>
+              <div class="text-subtitle1">{{ opis }}</div>
+            </q-card-section>
+            <q-card-section>
+              <q-btn
+                @click="$router.push('/komentari')"
+                label="Pogledaj komentare"
+              />
+            </q-card-section>
+            <q-card-section>
+              <div class="text-h6">Ocjena atrakcije:</div>
+              <div class="text-subtitle1">{{ ocjena }}</div>
+            </q-card-section>
+          </q-card>
         </q-layout-header>
         <q-layout-right>
-          <img alt="Veleri logo" src="~assets/naslovna.jpg" style="" />
+          <img alt="" src="~assets/naslovna.jpg" style="" />
         </q-layout-right>
       </q-layout>
     </q-page>
