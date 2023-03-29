60131d96f27acc226c89a26477661d7a2f2ff173
unknown <matijasickristian@gmail.com>
2023-03-29 17:35:27 +0200

    prvi front-end

--- turisticke/src/pages/IndexPage.vue
+++ turisticke/src/pages/IndexPage.vue
@@ -1,87 +1,53 @@
-<!-- <script setup>
-import { ref } from "vue"
-
-const imeGrada = ref("Rijeci");
-
-const resetGrada = () => {
-  imeGrada.value = "Rijeci"
-}
-
-</script>
-
 <template>
-  <H4>Veleučilište u {{ imeGrada }}</H4>
-
-  <input type="text" v-model="imeGrada">
-  <button @click="resetGrada()">Reset</button>
-</template> -->
-
-<script setup>
-import { useQuasar } from 'quasar'
-import { ref } from 'vue'
-
-const $q = useQuasar()
-
-const name = ref(null);
-const age = ref(null);
-const accept = ref(false);
-
-
-
-
-const onSubmit = () => {
-  if (accept.value !== true) {
-
-    console.log("U if je ušao")
-
-    $q.notify({
-      color: 'red-5',
-      textColor: 'white',
-      icon: 'warning',
-      message: 'Morate prihvatiti uvijete'
-    })
-  }
-  else {
-    $q.notify({
-      color: 'green-4',
-      textColor: 'white',
-      icon: 'cloud_done',
-      message: 'Spremljeno'
-    })
-  }
-}
-
-const onReset = () => {
-  name.value = null
-  age.value = null
-  accept.value = false
-}
-
-
-</script>
-
-<template>
-  <div class="q-pa-md" style="max-width: 400px">
-
-    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-      <q-input filled v-model="name" label="Vaše ime *" hint="Ime i prezime" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Unos je obavezan']" />
-
-      <q-input filled type="number" v-model="age" label="Vaše godine *" lazy-rules :rules="[
-        val => val !== null && val !== '' || 'Unesite vaše godine',
-        val => val > 0 && val < 100 || 'Mora bit više od 0 i manje od 100'
-      ]" />
-
-      <q-toggle v-model="accept" label="Prihvaćam uvjete" />
-
-      <div>
-        <q-btn label="Submit" type="submit" color="primary" />
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
-
+  <div class="bg-beige">
+    <q-header> </q-header>
+    <q-page>
+      <q-layout>
+        <q-layout-header>
+          <div class="q-pa-md">
+            <q-card>
+              <q-card-section>
+                <div class="text-h6">Naziv atrakcije:</div>
+                <div class="text-subtitle1">{{ naziv }}</div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6">Opis atrakcije:</div>
+                <div class="text-subtitle1">{{ opis }}</div>
+              </q-card-section>
+              <q-card-section>
+                <q-btn
+                  @click="$router.push('/komentari')"
+                  label="Pogledaj komentare"
+                />
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6">Ocjena atrakcije:</div>
+                <div class="text-subtitle1">{{ ocjena }}</div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </q-layout-header>
+        <q-layout-right>
+          <img alt="Veleri logo" src="~assets/naslovna.jpg" style="" />
+        </q-layout-right>
+      </q-layout>
+    </q-page>
   </div>
 </template>
 
-
-
+<script>
+export default {
+  data() {
+    return {
+      naziv: "",
+      opis: "",
+      komentar: "",
+      ocjena: "",
+      imageData: "",
+    };
+  },
+  created() {
+    // kod za povezivanje na bazu
+  },
+};
+</script>
