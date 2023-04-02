44bb1d19da002e698663aae0a090fc4831ad7aa0
Plb66 <126498700+Plb66@users.noreply.github.com>
2023-04-02 15:02:33 +0200

    doraden frontend za uson novih atrakcija

--- turisticke_atr/src/pages/IndexPage.vue
+++ turisticke_atr/src/pages/IndexPage.vue
@@ -1,17 +1,45 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/globe-vector-png-14.png"
-      style="width: 200px; height: 200px"
-    >
+  <q-page
+  padding
+   class="flex flex-center"
+  >
+  <div class="q-gutter-md" style="max-width: 350px">
+    <q-input v-model="inputText"
+    label="Naziv"
+    placeholder="Naziv atrakcije">
+   </q-input>
+
+    <q-input v-model="inputText"
+    label="Opis"
+    placeholder="Opis atrakcije">
+  </q-input>
+
+  <q-input v-model="inputText"
+    label="Lokacija"
+    placeholder="Lokacija atrakcije">
+  </q-input>
+
+  <q-uploader
+        url="http://localhost:4444/upload"
+        label="Slika atrakcije"
+        style="max-width: 300px"
+      />
+
+  <q-input v-model="inputText"
+    label="Širina"
+    placeholder="Grografska Širina atr">
+  </q-input>
+
+  <q-input v-model="inputText"
+    label="Dužina"
+    placeholder="Geografska dužina atr">
+  </q-input>
+</div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const inputText = ref('')
 </script>
