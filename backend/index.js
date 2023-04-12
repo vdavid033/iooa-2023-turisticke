53d837e5eb9f7aad89a944a6adf60a02493b67d7
nadiaalic <nadia.alic3@gmail.com>
2023-04-12 17:22:05 +0200

    API za unos - radi

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: false })); 
 const dbConfig = require("./dbConfig");
 
 
