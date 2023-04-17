c5be2eb3638eee0ad9a06c97dc473d7ed8213713
dstaresina <dstaresin@veleri.hr>
2023-04-17 17:13:18 +0200

    Povezana index stranica sa bazom

--- turisticke/src/pages/AxiosPageTest.vue
+++ turisticke/src/pages/AxiosPageTest.vue
@@ -11,6 +11,53 @@
       row-key="name"
     />
   </div>
+
+
+  <div class="q-pa-md">
+    <q-table
+      flat bordered
+      title="Podaci iz tablice ATRAKCIJE + edit"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+      binary-state-sort
+    >
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="id_atrakcije" :props="props">{{ props.row.id_atrakcije }}</q-td>
+          <q-td key="naziv" :props="naziv">
+            {{ props.row.naziv }}
+            <q-popup-edit v-model="props.row.naziv" title="Update naziva" buttons v-slot="scope">
+              <q-input type="text" v-model="scope.value" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="opis" :props="naziv">
+            {{ props.row.opis }}
+            <q-popup-edit v-model="props.row.opis" title="Update opisa" buttons v-slot="scope">
+              <q-input type="text" v-model="scope.value" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="slika" :props="props">{{ props.row.slika }}</q-td>
+          <q-td key="prosjecna_ocjena" :props="props">{{ props.row.prosjecna_ocjena }}</q-td>
+          <q-td key="geografska_sirina" :props="props">
+            {{ props.row.geografska_sirina }}
+            <q-popup-edit v-model="props.row.geografska_sirina" title="Update GEO širine" buttons persistent v-slot="scope">
+              <q-input type="number" v-model="scope.value" dense autofocus hint="Use buttons to close" />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="geografska_duzina" :props="props">
+            {{ props.row.geografska_duzina }}
+            <q-popup-edit v-model="props.row.geografska_duzina" title="Update GEO dužine" buttons persistent v-slot="scope">
+              <q-input type="number" v-model="scope.value" dense autofocus hint="Use buttons to close" />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="adresa" :props="props">{{ props.row.adresa }}</q-td>
+        </q-tr>
+      </template>
+    </q-table>
+  </div>
+
+
   <div class="q-pa-md q-gutter-sm">
     <q-btn color="primary" to="/" label="Natrag na početnu" />
   </div>
@@ -36,7 +83,7 @@ const columns = [
 
 const getPosts = async () => {
   try{
-    const response = await api.get('atrakcije', 'config')
+    const response = await api.get('atrakcije')
     console.log(response.data)
     posts.value = response.data
 
--- turisticke/src/pages/IndexPage.vue
+++ turisticke/src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md">
 
-    <q-card class="my-card" flat bordered>
+    <q-card v-for="post in posts" :key="post.id" class="my-card" flat bordered>
       <q-img src="~assets/trsat.jpg" />
 
       <q-card-section>
@@ -9,105 +9,62 @@
           to="one_atraction" />
 
         <div class="row no-wrap items-center">
-          <div class="col text-h6 ellipsis">Rijeka Trsat</div>
+          <div class="col text-h6 ellipsis">{{ post.naziv }}</div>
         </div>
 
-        <q-rating v-model="stars" :max="5" size="32px" />
+        <q-rating v-model=post.prosjecna_ocjena :max="5" size="32px" />
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        <div class="text-subtitle1">Trsatska Gradina</div>
+        <div class="text-subtitle1">{{ post.adresa }}</div>
         <div class="text-caption text-grey">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          {{post.opis}}
         </div>
       </q-card-section>
 
       <q-separator />
     </q-card>
 
-    <q-card class="my-card" flat bordered>
-      <q-img src="~assets/novi.jpg" />
 
-      <q-card-section>
-        <q-btn fab color="primary" icon="place" class="absolute"
-          style="top: 0; right: 12px; transform: translateY(-50%)" />
 
-        <div class="row no-wrap items-center">
-          <div class="col text-h6 ellipsis">Novi Vinodolski</div>
-        </div>
-
-        <q-rating v-model="stars" :max="5" size="32px" />
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        <div class="text-subtitle1">Pogled na grad Novi V.</div>
-        <div class="text-caption text-grey">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        </div>
-      </q-card-section>
-
-      <q-separator />
-    </q-card>
-
-    <q-card class="my-card" flat bordered>
-      <q-img src="~assets/ogulin.jpg" />
-
-      <q-card-section>
-        <q-btn fab color="primary" icon="place" class="absolute"
-          style="top: 0; right: 12px; transform: translateY(-50%)" />
-
-        <div class="row no-wrap items-center">
-          <div class="col text-h6 ellipsis">Ogulin</div>
-        </div>
-
-        <q-rating v-model="stars" :max="5" size="32px" />
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        <div class="text-subtitle1">Centar grada Ogulina</div>
-        <div class="text-caption text-grey">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        </div>
-      </q-card-section>
-
-      <q-separator />
-    </q-card>
-
-    <q-card class="my-card" flat bordered>
-      <q-img src="~assets/gola.jpg" />
-
-      <q-card-section>
-        <q-btn fab color="primary" icon="place" class="absolute" style="top: 0; right: 12px; transform: translateY(-50%)"
-          to="auth/test" />
-
-        <div class="row no-wrap items-center">
-          <div class="col text-h6 ellipsis">Gola</div>
-        </div>
-
-        <q-rating v-model="stars" :max="5" size="32px" />
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        <div class="text-subtitle1">Pogled sa tornja crkve u Goli</div>
-        <div class="text-caption text-grey">
-          Small plates, salads & sandwiches in an intimate setting.
-        </div>
-      </q-card-section>
-
-      <q-separator />
-    </q-card>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue"
+import {api} from 'boot/axios'
 
-const stars = ref(5);
+const posts = ref([])
+
+
+
+const getPosts = async () => {
+  try{
+    const response = await api.get('atrakcije')
+    console.log(response.data)
+    posts.value = response.data
+
+  }catch (error){
+    console.log(error)
+  }
+
+}
+
+onMounted(() => {
+  getPosts()
+})
+
+//const stars = ref(5);
 </script>
 
 <style>
 .bg-blue {
   background-color: #1e90ff;
   color: white;
+}
+
+.my-card {
+  width: 100%;
+  max-width: 300px;
 }
 </style>
