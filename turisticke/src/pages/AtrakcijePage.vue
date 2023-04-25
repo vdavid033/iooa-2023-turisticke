4a19cf466da2dada98c34d85ae083a8bd139f154
dstaresina <dstaresin@veleri.hr>
2023-04-25 12:14:45 +0200

    Sada ispravno prikazuje detalje atrakcija

--- turisticke/src/pages/AtrakcijePage.vue
+++ turisticke/src/pages/AtrakcijePage.vue
@@ -1,86 +1,65 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
 
-<q-card v-for="post in posts" :key="post.id" class="my-card" flat bordered>
-  <q-img :src=post.slika />
-  <q-card-section>
-
-    <div class="row no-wrap items-center">
-      <div class="col text-h1 ellipsis">{{ post.naziv }}</div>
+  <div v-for="post in posts" :key="post.id" class="row q-pa-md">
+    <div>
+      <div q-card >
+          <q-img :src=post.slika
+                  width="800px"
+                  height="600px"
+                  position="absolute"
+                  top="50%"
+                  left="50%"
+                  transform="translate(-50%, -50%)">
+            <div class="absolute-bottom text-subtitle1 text-center">
+              <div style="text-transform:uppercase; font-size:50px" >{{ post.naziv }}</div>
+            </div>
+          </q-img>
+      </div>
     </div>
 
-    <q-rating v-model=post.prosjecna_ocjena :max="5" size="32px" />
-  </q-card-section>
-
-  <q-card-section class="q-pt-none">
-    <div class="text-subtitle1">{{ post.adresa }}</div>
-    <div class="text-caption text-grey">
-      {{post.opis}}
+    <div class="q-pa-md">
+      <h4>Opis: {{ post.opis }}</h4>
+      <h4>Adresa: {{ post.adresa }}</h4>
+      <h4>Ocjena: {{ post.prosjecna_ocjena }}</h4>
+      <h4>Geo širina: {{ post.geografska_sirina }}</h4>
+      <h4>Geo dužina: {{ post.geografska_duzina }}</h4>
+      <h4>Adresa: {{ post.adresa }}</h4>
     </div>
-  </q-card-section>
-
-  <q-separator />
-</q-card>
-
-
-
-</div>
-
+  </div>
 
 </template>
 
 <script setup>
 
-import { ref, onMounted } from "vue"
-import {api} from 'boot/axios'
-import { useRoute, useRouter } from 'vue-router';
+  import { ref, onMounted } from "vue"
+  import {api} from 'boot/axios'
+  import { useRoute, useRouter } from 'vue-router';
 
-const posts = ref([])
-const route = useRoute()
-const router = useRouter()
+  const posts = ref([])
+  const route = useRoute()
+  const router = useRouter()
 
-const trenutniID = route.params.id
+  const trenutniID = route.params.id
 
-const getPosts = async () => {
-  try{
-    const response = await api.get(`/atrakcije/${trenutniID}`)
-    posts.value = response.data
+  const getPosts = async () => {
+    try{
+      const response = await api.get(`/atrakcije/${trenutniID}`)
+      posts.value = response.data
 
-    console.log("ID je: " , trenutniID)
-    console.log("Podatak iz baze po ID: " , posts.value)
+      console.log("ID je: " , trenutniID)
+      console.log("Podatak iz baze po ID: " , posts.value)
 
-
-  }catch (error){
-    console.log(error)
+    }catch (error){
+      console.log(error)
+    }
   }
-}
 
-onMounted(() => {
-  getPosts()
-})
+  onMounted(() => {
+    getPosts()
+  })
 
 </script>
 
-
-
 <style scoped>
-.bg-blue {
-  background-color: #1e90ff;
-}
 
-.q-truncate {
-  max-width: 600px;
-}
-
-.container {
-  display: grid;
-  align-items: center;
-  grid-template-columns: 1fr 1fr 1fr;
-  column-gap: 5px;
-}
-
-.my-card {
-  width: 100%;
-  max-width: 800px;
-}
 </style>
