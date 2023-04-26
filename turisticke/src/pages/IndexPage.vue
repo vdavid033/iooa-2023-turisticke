7c8095c980e4324683b5074e0977d2a3c141d99b
Dorian Halavuk <dhalavuk@mail.veleri.hr>
2023-04-26 21:11:15 +0200

    razmak od gumba do naziva

--- turisticke/src/pages/IndexPage.vue
+++ turisticke/src/pages/IndexPage.vue
@@ -4,29 +4,34 @@
     <q-card v-for="post in posts" :key="post.id" class="my-card" flat bordered>
       <q-img :src=post.slika />
 
+      
+
       <q-card-section>
         <q-btn fab color="primary" icon="place" class="absolute" style="top: 0; right: 12px; transform: translateY(-50%)"
           :to="'/one_atraction/' + post.id_atrakcije" />
 
-        <q-btn fab color="red" icon="delete" class="absolute" style="top: 0; left: 12px; transform: translateY(-50%)"
+          <q-btn fab color="red" icon="delete" class="absolute"  style="top: 0; left: 12px; transform: translateY(-50%)"
            @click="deleteById(post.id_atrakcije)" />
 
+           <div class="myDiv" style="padding: 10px;"></div>
 
         <div class="row no-wrap items-center">
           <div class="col text-h6 ellipsis">{{ post.naziv }}</div>
         </div>
-
+        
         <q-rating v-model=post.prosjecna_ocjena :max="5" size="32px" />
       </q-card-section>
-
+      
       <q-card-section class="q-pt-none">
         <div class="text-subtitle1">{{ post.adresa }}</div>
         <div class="text-caption text-grey">
           {{post.opis}}
         </div>
+       
       </q-card-section>
-
+      
       <q-separator />
+      
     </q-card>
 
 
