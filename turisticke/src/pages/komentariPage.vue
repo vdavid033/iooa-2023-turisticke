4117e79a823f25d4bbebab47d8e7f63d277f3feb
bornaiooa <bcrnkovic8@gmail.com>
2023-05-11 23:09:29 +0200

    moguć unos komentara i spremanje komentara u bazu

--- turisticke/src/pages/komentariPage.vue
+++ turisticke/src/pages/komentariPage.vue
@@ -1,61 +1,51 @@
 <template>
   <div style="background-color: #8CDEED;">
 
-  <h3>Unos komentara</h3>
-  <h6>U polje ispod upišite svoj komentar o atrakciji</h6>
-  <q-form class="text">
-      <!-- <textarea v-model="newComment" placeholder="Unesite svoj komentar..."></textarea> -->
-      <q-input filled v-model="noviKomentar" label="Unesi svoj komentar.. " :dense="dense" />
+      <h3>Unos komentara</h3>
+      <h6>U polje ispod upišite svoj komentar o atrakciji</h6>
+      <q-form class="text">
+          <q-input filled v-model="komentar" label="Unesi svoj komentar.. " :dense="dense" />
+      </q-form>
+      <q-card-section>
+          <q-btn label="Dodaj komentar" @click="dodajKomentar(komentar, trenutniID)" />
+      </q-card-section>
 
-    </q-form>
-
-    
-    <q-card-section>  
-      <q-btn label="Dodaj komentar"
-          @click="submitForm"/>
-    </q-card-section>
-
-
-  <q-card-section>
-      <q-btn color="#4CAF50" @click="$router.push('/')" label="Natrag na početnu" />
-    </q-card-section>
-    
-
+      <q-card-section>
+          <q-btn color="#4CAF50" @click="$router.push('/')" label="Natrag na početnu" />
+      </q-card-section>
   </div>
 </template>
 
-<script>
-import { ref } from 'vue'
-import {api} from 'boot/axios' 
-export default {
-  data() {
-    return {
-      noviKomentar: ''
-    }
-  },
-  methods: {
- 
-    async submitForm () {
-      const sampleData = {
-        noviKomentar:this.noviKomentar
-      }
-      try {
-        const response = await api.post(
-          '/komentari',
-          sampleData
-        )
-        console.log(response.data)
-        // this.showDialog = true
-        // this.resetForm()
-      } catch (error) {
-        console.error(error)
-      }
-    }
+<script setup>
+import { ref, onMounted } from "vue"
+import { api } from 'boot/axios'
+
+import { useRoute, useRouter } from 'vue-router';
+
+const posts = ref([])
+const route = useRoute()
+const router = useRouter()
+const trenutniID = route.params.id
+
+
+//Dodavanje komentara za atrakciju
+
+const dodajKomentar = async (komentar, trenutniID) => {
+  try {
+      console.log('Komentar: ', komentar)
+      console.log("ID: ", trenutniID)
+
+      const response = await api.post(`http://localhost:4200/dodajKomentar/${trenutniID}`, {
+          Komentar: komentar
+      });
+      console.log(response.data);
+  }
+  catch (error) {
+      console.log(error);
   }
 }
+
 </script>
-
-
 
 <style scoped>
 .text {
@@ -67,8 +57,8 @@ export default {
   border: 1px solid #ccc;
   border-radius: 5px;
   background-color: rgb(228, 233, 234);
-  
-  
+
+
 }
 
 button {
@@ -78,7 +68,7 @@ button {
   border-radius: 5px;
   padding: 10px 20px;
   cursor: pointer;
-  
+
 }
 
 button:hover {
