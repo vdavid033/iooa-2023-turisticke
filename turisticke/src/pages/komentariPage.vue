f060123075fca0bc174244f79426a16434f7227e
bornaiooa <bcrnkovic8@gmail.com>
2023-05-11 23:18:13 +0200

    moguć unos komentara i spremanje komentara u bazu

--- turisticke/src/pages/komentariPage.vue
+++ turisticke/src/pages/komentariPage.vue
@@ -1,18 +1,24 @@
 <template>
   <div style="background-color: #8CDEED;">
 
-      <h3>Unos komentara</h3>
-      <h6>U polje ispod upišite svoj komentar o atrakciji</h6>
-      <q-form class="text">
-          <q-input filled v-model="komentar" label="Unesi svoj komentar.. " :dense="dense" />
-      </q-form>
-      <q-card-section>
-          <q-btn label="Dodaj komentar" @click="dodajKomentar(komentar, trenutniID)" />
-      </q-card-section>
+  <h3>Unos komentara</h3>
+  <h6>U polje ispod upišite svoj komentar o atrakciji</h6>
+  <q-form class="text">
 
-      <q-card-section>
-          <q-btn color="#4CAF50" @click="$router.push('/')" label="Natrag na početnu" />
-      </q-card-section>
+      <q-input filled v-model="komentar" label="Unesi svoj komentar.. " :dense="dense" />
+    </q-form>
+
+    
+    <q-card-section>
+            <q-btn label="Dodaj komentar" @click="dodajKomentar(komentar, trenutniID)" />
+        </q-card-section>
+
+
+  <q-card-section>
+      <q-btn color="#4CAF50" @click="$router.push('/')" label="Natrag na početnu" />
+    </q-card-section>
+    
+
   </div>
 </template>
 
@@ -31,21 +37,21 @@ const trenutniID = route.params.id
 //Dodavanje komentara za atrakciju
 
 const dodajKomentar = async (komentar, trenutniID) => {
-  try {
-      console.log('Komentar: ', komentar)
-      console.log("ID: ", trenutniID)
+    try {
+        console.log('Komentar: ', komentar)
+        console.log("ID: ", trenutniID)
 
-      const response = await api.post(`http://localhost:4200/dodajKomentar/${trenutniID}`, {
-          Komentar: komentar
-      });
-      console.log(response.data);
-  }
-  catch (error) {
-      console.log(error);
-  }
+        const response = await api.post(`http://localhost:4200/dodajKomentar/${trenutniID}`, {
+            Komentar: komentar
+        });
+        console.log(response.data);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
-
 </script>
+
 
 <style scoped>
 .text {
@@ -57,8 +63,8 @@ const dodajKomentar = async (komentar, trenutniID) => {
   border: 1px solid #ccc;
   border-radius: 5px;
   background-color: rgb(228, 233, 234);
-
-
+  
+  
 }
 
 button {
@@ -68,7 +74,7 @@ button {
   border-radius: 5px;
   padding: 10px 20px;
   cursor: pointer;
-
+  
 }
 
 button:hover {
